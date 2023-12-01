629f64c0f34fb6804b99be61b179bc44c751a3f6
Antonio Barboza <ant-2-onio@hotmail.com>
2023-12-01 08:23:43 -0300

    feat: engine camera

--- core/engine.py
+++ core/engine.py
@@ -1,12 +1,23 @@
-class Vertice3D:
-  def __init__(self, x, y=None, z=None):
-    if type(x) == tuple or type(x) == list and len(x) == 3:
-      self.x, self.y, self.z = x
-    else:
-      self.x, self.y, self.z = x, y, z
-  
-  def __repr__(self):
-    return f'Vector3D({self.x},{self.y},{self.z})'
+import numpy as np
+
+class Camera:
+  def __init__(self, hx, hy, d, c, n, v):
+    self.view_plane = (hx, hy)
+    self.d = d
+    self.c = np.array(c)
+    self.n = np.array(n)
+    self.v = np.array(v)
+    self.v_ = self.v - np.dot(self.v, self.n) / np.dot(self.n, self.n) * self.n
+    self.u = np.cross(self.n, self.v_)
+    self._base = np.array([
+      self.normalize(self.u), self.normalize(self.v_), self.normalize(self.n)
+    ])
+
+  def normalize(self, v):
+    return v / np.linalg.norm(v)
+
+  def change_base(self, v):
+    return np.dot(self._base, v - self.c)
 
 class Object3D:
   def __init__(self, vertices, triangles):
@@ -15,3 +26,9 @@ class Object3D:
   
   def __repr__(self):
     return f'Object3D({len(self.vertices)},{len(self.triangles)})'
+
+  def triangle(self, index: int):
+    return [self.vertices[i-1] for i in self.triangles[index]]
+
+  def __iter__(self):
+    return iter(self.triangles)
--- tests.py
+++ tests.py
@@ -1,5 +1,16 @@
 from core import io
+from core.engine import Camera
+import numpy as np
 
 objects = list(io.load_objects_byu('assets/objects/triangulo.byu'))
 
-print(objects)
+camera = Camera(
+  hx=1,
+  hy=1,
+  d=1,
+  c=[1, 1, 2],
+  n=[-1, -1, -1],
+  v=[0, 0, 1]
+)
+
+print(camera.change_base(np.array([1, -3, -5])))
